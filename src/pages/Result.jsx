020f7e7ca977b218ac0e9d474af1a9d401b6470d
Minji Yun <gee2450@kookmin.ac.kr>
2023-06-06 19:39:32 +0900

    Remove never used code and use useMemo - wrapping problems and typeArray

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate, useLocation } from "react-router-dom";
 import StyledArticle from '../component/Article';
