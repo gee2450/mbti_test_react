81c22146ead0bca92b6333766c02047b06c9eecd
Minji Yun <gee2450@kookmin.ac.kr>
2024-03-10 15:41:00 +0900

    Add switch language function - only start page because data not prepared with English

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Start from './pages/Start';
 import Test from './pages/Test';
 import Result from './pages/Result';
 import { BrowserRouter } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import i18n from './lang/i18n.ts';
 
 function App() {
+  // 언어 변경하기
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+  
   return (
     <BrowserRouter>
+      <Button onClick={() => changeLanguage("en")}>
+        English
+      </Button>
+      <Button onClick={() => changeLanguage("ko")}>
+        Korean
+      </Button>
       <Routes>
         <Route path="/" element={<Start />}></Route>
         <Route path="/test" element={<Test />}></Route>
